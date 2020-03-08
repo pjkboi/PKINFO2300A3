26084deea5ffdb514a83c2bcf6095189896db1f2
PJ Khamvongsa <pjkhamvongsa@gmail.com>
2020-03-08 17:41:02 -0400

    created the basic counter

---
+++ pkinfo2300/src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+   <h1>Black Jack Counter App</h1>
+   <h3>{{ counter }}</h3>
+   <button class="button" @click="counter++">+</button>
+   <button class="button" @click="counter--">-</button>
+  </div>
+</template>
+<script>
+export default {
+ data(){
+  return{
+   counter: 0
+  }
+ }
+ 
+}
+</script>
+<style scoped>
+ .button{
+    border: 2px solid black;
+  background-color: white;
+  color: black;
+  padding: 14px 28px;
+  font-size: 16px;
+  cursor: pointer;
+ }
+</style>
